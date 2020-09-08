bbf59ce6ab14338ee4bda868ef3d4abb8670c29d
StanTarasenko <stan.tarasenko1991@gmail.com>
2020-09-08 11:40:57 +0300

    youngEmployees - function that use method ".filter " & ".sort ",'.filter ' checking the list of the objects inside massive and find people by age. Function '.sort ' able to bring back to a massive only that info that you will point. To exsample only " [youngEmployees.map(({name}) => (name))].sort() "

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+let vasia = { name: "Вася", age: 25 };
+let petia = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+let egor = { name: "Егор", age: 40 };
+let dima = { name: "Дима", age: 35 };
+let lena = { name: "Лена", age: 18 };
+
+let users = [ vasia, petia, masha, egor, dima, lena ];
+
+let youngEmployees = users.filter(function(user){
+    return user.age < 30
+})
+
+    
+const result = [youngEmployees.map(({name}) => (name))].sort()
+console.log(result);
